d677e29a72df5313bec7c2704d3c1fb10f7daf61
Ismail Hussain <78728394+ihussain0910@users.noreply.github.com>
2024-05-03 12:48:11 +0100

    Update biokwondo.py

--- biokwondo.py
+++ biokwondo.py
@@ -8,6 +8,19 @@ import feedback_generation
 import mp_helpers
 import pyttsx3
 
+
+# We follow the ooutlined framework provided by MediaPipe https://developers.google.com/mediapipe, for detection, pre prcocessing and frame reading
+
+def prob_viz(res, actions, input_frame, colors):
+    output_frame = input_frame.copy()
+    for num, prob in enumerate(res):
+        cv2.rectangle(output_frame, (0,60+num*40), (int(prob*100), 90+num*40), colors[0], -1)
+        cv2.putText(output_frame, action[num], (0, 85+num*40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+        
+    return output_frame
+
+
+
 colors = [(245,117,16), (117,245,16), (16,117,245)]
 def prob_viz(res, actions, input_frame, colors):
     output_frame = input_frame.copy()
